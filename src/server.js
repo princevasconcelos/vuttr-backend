a638da390146bd64442bf787da7101f98056bb02
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-09 23:25:45 -0300

    use router

--- src/server.js
+++ src/server.js
@@ -1,27 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const PORT = 3000;
 
-app.get("/tools", (req, res) => {
-  res.status(200).send({ msg: "showing all tools" });
-});
-
-app.get("/tools/:search", (req, res) => {
-  res.status(200).send({
-    msg: "1 result found"
-  });
-});
-
-app.post("/tools", (req, res) => {
-  res.status(201).send({
-    msg: "created"
-  });
-});
-
-app.delete("/tools/:id", (req, res) => {
-  res.status();
-});
+app.use(require('./router'));
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`node runnig on port ${PORT}`);
