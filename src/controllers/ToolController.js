a5aec0193889b1abe57d48c597424eba25409a81
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-10 01:04:34 -0300

    implement deleteTool

--- src/controllers/ToolController.js
+++ src/controllers/ToolController.js
@@ -1,29 +1,27 @@
-const toolMock = require('../../mocks/tools');
 const Tool = require('../models/Tool');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const ToolController = {
-  getTools(req, res) {
+  async getTools(req, res) {
     const {q, tag} = req.query;
+    const tools = await Tool.find();
 
     if (q) {
       return res
           .status(200)
           .json(
-              toolMock.filter((t) =>
-                t.title.toLowerCase().includes(q.toLowerCase())
-              )
+              tools.filter((t) => t.title.toLowerCase().includes(q.toLowerCase()))
           );
     }
 
     if (tag) {
       return res
           .status(200)
-          .json(
-              toolMock.filter((tool) => tool.tags.find((t) => t.includes(tag)))
-          );
+          .json(tools.filter((tool) => tool.tags.find((t) => t.includes(tag))));
     }
 
-    return res.status(200).json(toolMock);
+    return res.status(200).json(tools);
   },
 
   async createTool(req, res) {
@@ -39,8 +37,28 @@ const ToolController = {
     return res.status(201).json(tool);
   },
 
-  deleteTool(req, res) {
-    return res.status(204).send();
+  async deleteTool(req, res) {
+    const {id} = req.params;
+
+    if (!id) {
+      return res.status(400).json({
+        message: isProduction
+          ? 'Bad request'
+          : 'Provide an ID with the URL /tools/:id',
+      });
+    }
+
+    try {
+      await Tool.findByIdAndDelete(id);
+
+      return res.status(204).json();
+    } catch (err) {
+      return res.status(404).json({
+        error: isProduction
+          ? 'Not found'
+          : `No tool with that id: ${id}. Check if the ID is correct.`,
+      });
+    }
   },
 };
 
