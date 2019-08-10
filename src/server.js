eadfc0986331afba34e73bf42f977c4b04bcf4a8
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-09 23:44:56 -0300

    add mongoose and connect

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const PORT = 3000;
+
+mongoose.connect(
+    'mongodb+srv://princevasconcelos:princevasconcelos@cluster0-t4dtv.mongodb.net/tools?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+    }
+);
 
 app.use(require('./router'));
 
