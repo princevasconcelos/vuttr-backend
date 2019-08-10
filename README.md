577349d38ae09689fe045f01851424be12626d3c
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-09 21:12:55 -0300

    add eslint and nodemon

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Very Useful Tools to Remember
+
+I'm a frontend developer who study backend in the free time, so my knowledges in this field aren't good, but at least i try :)  
+
+## Stack
+
+- Nodejs
+- Express
+- Mongodb
--- src/server.js
+++ src/server.js
@@ -3,8 +3,24 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("its working");
+app.get("/tools", (req, res) => {
+  res.status(200).send({ msg: "showing all tools" });
+});
+
+app.get("/tools/:search", (req, res) => {
+  res.status(200).send({
+    msg: "1 result found"
+  });
+});
+
+app.post("/tools", (req, res) => {
+  res.status(201).send({
+    msg: "created"
+  });
+});
+
+app.delete("/tools/:id", (req, res) => {
+  res.status();
 });
 
 app.listen(process.env.PORT || PORT, () => {
