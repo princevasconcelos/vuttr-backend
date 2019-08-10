1236485b31c7e86c775601f3b561df711e5c18f8
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-10 11:57:48 -0300

    export mongo config to a file

--- src/server.js
+++ src/server.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('./config/mongo');
 
 const PORT = 3000;
-
-mongoose.connect(
-    'mongodb+srv://princevasconcelos:princevasconcelos@cluster0-t4dtv.mongodb.net/tools?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-    }
-);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
