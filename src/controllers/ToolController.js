4aa6e35b7d4ef60d1dec4bfd064f1cf7d8fd0a0d
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-09 23:26:38 -0300

    implement getTools

--- src/controllers/ToolController.js
+++ src/controllers/ToolController.js
@@ -1,6 +1,28 @@
+const toolMock = require('../../mocks/tools');
+
 const ToolController = {
   getTools(req, res) {
-    return res.status(200).send('retrieve all');
+    const {q, tag} = req.query;
+
+    if (q) {
+      return res
+          .status(200)
+          .send(
+              toolMock.filter((t) =>
+                t.title.toLowerCase().includes(q.toLowerCase())
+              )
+          );
+    }
+
+    if (tag) {
+      return res
+          .status(200)
+          .send(
+              toolMock.filter((tool) => tool.tags.find((t) => t.includes(tag)))
+          );
+    }
+
+    return res.status(200).send(toolMock);
   },
 
   createTool(req, res) {
