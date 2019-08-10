4565da37122405c707cc1e7543892ec629b79049
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-08-10 01:05:10 -0300

    remove version key from tool schema

--- src/models/Tool.js
+++ src/models/Tool.js
@@ -1,17 +1,20 @@
 const {Schema, model} = require('mongoose');
 
-const ToolSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  tags: [String],
-});
+const ToolSchema = new Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      link: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+      tags: [String],
+    },
+    {versionKey: false}
+);
 
 module.exports = model('Tool', ToolSchema);
